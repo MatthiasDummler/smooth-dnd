382ce3dcc24dedeb89081abcf38da163430db178
MatthiasDummler <matthias.dummler@cordaware.com>
2021-12-23 14:18:47 +0100

    Version: 0.13.0

---
+++ dist/src/exportTypes.d.ts
@@ -0,0 +1,73 @@
+import { IContainer, Position } from "./interfaces";
+export interface SmoothDnD {
+    dispose: () => void;
+    setOptions: (options: ContainerOptions, merge?: boolean) => void;
+}
+export declare type SmoothDnDCreator = ((element: HTMLElement, options?: ContainerOptions) => SmoothDnD) & {
+    dropHandler?: any;
+    wrapChild?: boolean;
+    maxScrollSpeed?: number;
+    useTransformForGhost?: boolean;
+    cancelDrag: () => void;
+    isDragging: () => boolean;
+};
+declare type Callback<T> = (params: T) => void;
+export interface DropResult {
+    removedIndex: number | null;
+    addedIndex: number | null;
+    payload?: any;
+    element?: HTMLElement;
+}
+export interface DropPlaceholderOptions {
+    className?: string;
+    animationDuration?: number;
+    showOnTop?: boolean;
+}
+export interface DragStartParams {
+    isSource: boolean;
+    payload: any;
+    willAcceptDrop: boolean;
+}
+export interface DragEndParams {
+    isSource: boolean;
+    payload: any;
+    willAcceptDrop: boolean;
+}
+export declare type DragStartCallback = Callback<DragStartParams>;
+export declare type DragEndCallback = Callback<DragEndParams>;
+export declare type OnDropCallback = Callback<DropResult>;
+export declare type OnDropReadyCallback = Callback<DropResult>;
+export declare type OnStyleContainerCallback = Callback<{
+    property: string;
+    value: string | null;
+    element: HTMLElement;
+}>;
+export declare type CreateGhostElement = (wrapperElement: HTMLElement, position: Position, container: IContainer, cursor: string) => HTMLElement;
+export interface ContainerOptions {
+    behaviour?: 'move' | 'copy' | 'drop-zone' | 'contain';
+    groupName?: string;
+    orientation?: 'vertical' | 'horizontal';
+    dragHandleSelector?: string;
+    nonDragAreaSelector?: string;
+    dragBeginDelay?: number;
+    animationDuration?: number;
+    autoScrollEnabled?: boolean;
+    lockAxis?: 'x' | 'y';
+    dragClass?: string;
+    dropClass?: string;
+    onDragStart?: DragStartCallback;
+    onDrop?: OnDropCallback;
+    onStyleContainer?: OnStyleContainerCallback;
+    getChildPayload?: (index: number) => any;
+    shouldAnimateDrop?: (sourceContainerOptions: ContainerOptions, payload: any) => boolean;
+    shouldAcceptDrop?: (sourceContainerOptions: ContainerOptions, payload: any) => boolean;
+    onDragEnter?: () => void;
+    onDragLeave?: () => void;
+    onDropReady?: OnDropReadyCallback;
+    removeOnDropOut?: boolean;
+    getGhostParent?: () => HTMLElement;
+    onDragEnd?: DragEndCallback;
+    createGhostElement?: CreateGhostElement;
+    dropPlaceholder?: DropPlaceholderOptions | boolean;
+}
+export {};
